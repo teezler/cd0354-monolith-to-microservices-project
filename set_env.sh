d957a2c6301a5940c66b33b68896128e3cc53866
teezler <lanreola.tomi@gmail.com>
2022-08-07 14:17:03 +0100

    updated travis.yml file

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgresAdmin
+export POSTGRES_PASSWORD=asdf1234
+export POSTGRES_HOST=postgres.ctxmvvmqimqn.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=mypicbucket-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
